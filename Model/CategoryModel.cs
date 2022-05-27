b43dd25e3e0d0d91371ca21780c7b056a4c4e29c
alayrathod12 <71247276+alayrathod12@users.noreply.github.com>
2022-05-27 18:49:10 +0530

    Add files via upload

---
+++ Model/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDoperationmvc.Models.Models_cat
+{
+    public class CategoryModel
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
