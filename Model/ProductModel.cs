899ec5e4f6306024048c6da0ac5dce8626ad6d6e
alayrathod12 <71247276+alayrathod12@users.noreply.github.com>
2022-05-27 18:49:33 +0530

    Add files via upload

---
+++ Model/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDoperationmvc.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+       
+    }
+}
